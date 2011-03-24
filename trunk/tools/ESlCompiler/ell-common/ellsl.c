aade09cd40a7e6a6cbb6d2fb4f81b34370df17e8
jelo.wang@gmail.com <jelo.wang@gmail.com@f1f07ddf-9760-0243-9034-4344de907ec5>
2011-03-24 14:59:03 +0000

    add ESLCompiler source.

---
+++ trunk/tools/ESlCompiler/ell-common/ellsl.c
@@ -0,0 +1,263 @@
+
+/*
+
++	Executable Linking-Library 1.0.0.
++	Architecture : ARMv6
+
++	'Executable Linking-Library' is a Dynamic Linking solution for closed runing environment.
++	The project lunched by Jelo Wang since 2010 from 'Techniques of Knowledge' community. 
+
++	You can redistribute it and/or modify it under the terms of the gnu general public version 3 of 
++	the license as published by the free software foundation.this program is distributed in the hope 
++	that it will be useful,but without any warranty.without even the implied warranty of merchantability 
++	or fitness for a particular purpose.																					
+																												
++	(C)	突壳开源Techniques of Knowledge
++		an open source community since 2008
++		Community : http://www.tok.cc
++		Contact Us : jelo.wang@gmail.com
+
++		技术支持、功能扩展、平台搭建，欢迎与我们联系。
++		我们将为您提供有偿的，强力的服务。
+
+*/
+
+# include "ellhal.h"
+# include "ellsl.h"
+
+int EllSlListCreate () {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : create a linked-list
+
+	ELLSLLIST* list = (ELLSLLIST* ) EllMalloc ( sizeof(ELLSLLIST) ) ;
+
+	if ( 0 == list ) return 0 ;
+
+	list->element = 0 ;
+	list->head = 0 ;
+	list->next = 0 ;
+
+	return (int)list ;
+
+}
+
+int EllSlListInsert ( int lt , int element ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : add a node
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+	ELLSLLIST* newn = 0 ;
+
+	if ( 0 == list ) return 0 ;
+
+	newn = (ELLSLLIST* ) EllMalloc ( sizeof(ELLSLLIST) ) ;
+
+	if ( 0 == newn ) return 0 ;
+
+	newn->element = element ;
+	newn->head = 0 ;
+	newn->next = 0 ;
+
+	if ( 0 == list->head ) {
+		list->head = newn ;
+		list->next = newn ;
+	} else {
+		list->next->next = newn ;
+		list->next = newn ;
+	}
+	
+	return 1 ;
+
+}
+
+int EllSlListDelete ( int lt , int element ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : delete a node	
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+	ELLSLLIST* walker = 0 ;
+	ELLSLLIST* pre = 0 ;
+
+	if ( 0 == list ) return 0 ;
+
+	for ( walker = list->head , pre = walker ; walker && walker->element != element ; pre = walker , walker = walker->next ) ;
+
+	if ( walker ) {
+		pre->next = walker->next ; 
+		EllFree ( walker ) ;
+	}
+
+	return 1 ;
+
+}
+
+int EllSlListDestroy ( int lt ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : destroy a list
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+	ELLSLLIST* walker = 0 ;
+
+	if ( 0 == list ) return 1 ;
+
+	for ( walker = list->head ; walker ; ) {
+
+		list->next = walker->next ; 
+		if ( walker->element ) EllFree ( (void*) walker->element ) ;
+		EllFree ( walker ) ;
+		walker = list->next ; 
+
+	}
+
+	EllFree ( list ) ;
+
+	return 1 ;
+
+}
+
+
+int EllSlListSetIterator ( int lt , int position ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : set an iterator
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+	ELLSLLIST* walker = 0 ;
+
+	if ( 0 == list ) return 0 ;
+	
+	if ( ELLSLSEEK_HEAD == position ) {
+		//	将迭代器指向头节点
+		list->next = list->head ; 
+	} else if ( ELLSLSEEK_TAIL == position ) {
+		//	将迭代器指向尾节点
+		for ( walker = list->head ; walker && walker->next ; walker = walker->next ) ;
+		if ( walker ) list->next = walker ;
+	}
+
+	return 1 ; 
+
+}
+
+int EllSlListIteratorPermit ( int lt ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : 判断一个迭代器是否可以继续运行
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+	ELLSLLIST* walker = 0 ;
+
+	if ( 0 == list ) return 0 ;
+	if ( 0 == list->next ) return 0 ;
+
+	return 1 ; 
+
+}
+
+int EllSlListIteratorGetElement ( int lt ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : get an element
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+
+	if ( 0 == list ) return 0 ;
+	if ( 0 == list->next ) return 0 ;
+
+	return list->next->element ;
+
+}
+
+int EllSlListIteratorNext ( int lt ) {
+
+	//	author : Jelo Wang
+	//	since : 20100609
+	//	(C)TOK
+
+	//	notes : iterator next
+
+	ELLSLLIST* list = (ELLSLLIST* ) lt ;
+
+	if ( 0 == list ) return 0 ;
+	if ( 0 == list->next ) return 0 ;
+
+	list->next = list->next->next ; 
+	
+	return 1 ;
+
+}
+
+int EllSlGetString ( int file , int position , char* name ) {
+	
+	//	author : Jelo Wang
+	//	since : 20100624
+	//	(C)TOK
+
+	//	notes : get string from file
+	
+	int walker = 0 ;
+	char swap ;
+
+	if( !file ) return 0 ;
+
+	EllHalFileSeek ( file , position , ELLHAL_SEEK_HEAD ) ;
+
+# ifdef MTK_ELL
+
+	for(  ; ; walker++ ) {
+	
+		EllHalFileRead ( file , &swap , 1 , 1 ) ;
+
+		if( 0x20 == swap || 0 == swap ) break ;
+		
+		name[walker] = swap ;
+	
+	}
+
+# else
+
+	for(  ; !EllHalFileEnd ( file ) ; walker++ ) {
+
+		EllHalFileRead ( file , &swap , 1 , 1 ) ;
+
+		if( 0x20 == swap || 0 == swap ) break ;
+		
+		name[walker] = swap ;
+
+	}
+
+# endif
+
+	name[walker] = 0 ;
+
+	return 1 ;
+
+}
+
+
+
---
+++ trunk/tools/ESlCompiler/ell-hal/ellhal.h
@@ -0,0 +1,77 @@
+
+/*
+
++	Executable Linking-Library 1.0.0.
++	Architecture : ARMv6
+
++	'Executable Linking-Library' is a Dynamic Linking solution for closed runing environment.
++	The project lunched by Jelo Wang since 2010 from 'Techniques of Knowledge' community. 
+
++	You can redistribute it and/or modify it under the terms of the gnu general public version 3 of 
++	the license as published by the free software foundation.this program is distributed in the hope 
++	that it will be useful,but without any warranty.without even the implied warranty of merchantability 
++	or fitness for a particular purpose.																					
+																												
++	(C)	突壳开源Techniques of Knowledge
++		an open source community since 2008
++		Community : http://www.tok.cc
++		Contact Us : jelo.wang@gmail.com
+
++		技术支持、功能扩展、平台搭建，欢迎与我们联系。
++		我们将为您提供有偿的，强力的服务。
+
+*/
+
+# ifndef __ELLHAL
+# define __ELLHAL
+
+typedef enum {
+
+	//	author : Jelo Wang
+	//	since : 20100703
+	//	(C)TOK	
+
+	//	seek
+	//	opening of read only
+	//	opening of create and write
+	
+	ELLHAL_SEEK_HEAD ,
+	ELLHAL_READ_OPEN ,
+	ELLHAL_READW_OPEN ,
+	ELLHAL_CREW_OPEN ,
+
+} ELLHAL_FILE ;
+
+extern void* EllNormalloc ( long int length , char* file , int line )  ;
+
+# define EllMalloc(length) EllNormalloc(length,__FILE__,__LINE__)
+
+typedef struct MMN {
+
+	//	author : Jelo Wang
+	//	since : 20100417
+	//	(C)TOK
+	
+	char* file ;
+	int line ;
+	int length ;
+	int address ;
+	
+	struct MMN* head ;
+	struct MMN* next ;
+	
+} MEMORY_MONITOR ;
+
+# endif
+
+extern int EllFree ( void* buffer ) ;
+extern int EllFreeEx ( void** buffer ) ;
+extern int EllHalMemoryLeaked () ;
+extern int EllHalFileOpen ( char* path , int flag ) ;
+extern int EllHalFileSeek( int file , int offset , int direct ) ;
+extern int EllHalFileRead ( int file , void* buffer , int size , int counter ) ;
+extern int EllHalFileWrite ( int file , void* buffer , int size , int counter ) ;
+extern int EllHalFileEnd ( int file ) ;
+extern int EllHalFileLength( int file ) ;
+extern int EllHalFileClose ( int file ) ;
+extern void EllMemcpy ( void* target_memory , void* source_memory , int length ) ;
